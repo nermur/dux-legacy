f6d003989b152222f341149ce997df43b77ea2d1
nermur <29991266+nermur@users.noreply.github.com>
2022-05-31 05:53:59 -0700

    Update 02-post_chroot_root.sh

--- scripts/02-post_chroot_root.sh
+++ scripts/02-post_chroot_root.sh
@@ -144,6 +144,9 @@ strace usbutils linux-firmware gnome-keyring avahi nss-mdns \
 man-db man-pages pacman-contrib snapper snap-pac mkinitcpio linux-zen linux-zen-headers bat \
 wget trash-cli reflector rebuild-detector vim "
 
+[[ ! -d "/sys/firmware/efi" ]] &&
+	declare -r bootloader_type="1" && export bootloader_type
+
 [[ ${bootloader_type} -eq 1 ]] &&
 	PKGS+="grub os-prober "
 [[ ${bootloader_type} -eq 2 ]] &&
@@ -200,9 +203,6 @@ _systemctl enable ${SERVICES}
 
 [[ ${disks_lvm2} -eq 0 ]] &&
 	systemctl mask lvm2-lvmpolld.socket lvm2-monitor.service
-
-[[ ! -d "/sys/firmware/efi" ]] &&
-	declare -r bootloader_type="1" && export bootloader_type
 
 [[ ${disable_cpu_security_mitigations} -eq 0 ]] &&
 	MITIGATIONS_OFF="mitigations=off"
