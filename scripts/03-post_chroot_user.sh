1ff9bb065b207338856275fcd363aa394d85ba15
nermur <29991266+nermur@users.noreply.github.com>
2022-06-04 21:47:39 -0700

    Update 03-post_chroot_user.sh

--- scripts/03-post_chroot_user.sh
+++ scripts/03-post_chroot_user.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${SCRIPT_DIR}" && GIT_DIR=$(git rev-parse --show-toplevel)
 source "${GIT_DIR}/scripts/GLOBAL_IMPORTS.sh"
 source "${GIT_DIR}/configs/settings.sh"
+WHICH_USER="${INITIAL_USER}"
 
 clear
 
